98f3e111a3bf07ad6c7481b76e3c5759dfc1eaa3
Hafsa <148873058+hafsahtx@users.noreply.github.com>
2025-05-05 17:19:23 +0100

    Create function operate

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ function multiply(a,b){
 function divide(a,b){
     return a/b;
 }
+function operate(first,second,operator){
+    switch(operator){
+        case "+":
+            return add(first,second);
+        case "-":
+            return subtract(first,second);
+        case "*":
+            return multiply(first,second);
+        case "/":
+            return divide(first,second);
+        default:
+            console.log("use either + - * /");
+            
+    }
+}
+
+let firstOperand = 3;
+let secondOperand = 5;
+let operator = "+";
+console.log(operate(firstOperand,secondOperand,operator));
