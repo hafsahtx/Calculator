08c3a8d6104ab8f86dc00349b72d4021725a0de7
Hafsa <148873058+hafsahtx@users.noreply.github.com>
2025-05-14 14:21:07 +0100

    Add listener to buttons and retrieve which button clicked

--- script.js
+++ script.js
@@ -37,8 +37,11 @@ function createButtons(){
     }
     
 }
-
 createButtons();
+const btn = Array.from(document.querySelectorAll("button"));
+btn.forEach(button=>button.addEventListener("click",(e)=>{console.log(e.target.textContent)}));
+
+
 
 
 
